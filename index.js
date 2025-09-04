e6a1d5d39c5ef75098f3becc3e45dbf5fc43f985
edwardzh976 <84339439+edwardzh976@users.noreply.github.com>
2025-09-04 15:53:11 +0800

    overhaul

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const txt = "Aspiring software developer"
-var i = 0;
-var speed = 100;
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("text_out").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
-window.onload = typeWriter();
 
-AOS.init();
 
 
